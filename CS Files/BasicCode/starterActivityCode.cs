f7444258e1cfcf5e375f04e892c55c6059d55829
Owen <owenobrien92007@gmail.com>
2025-11-02 14:23:00 +0000

    Create starterActivityCode.cs

---
+++ CS Files/BasicCode/starterActivityCode.cs
@@ -0,0 +1,66 @@
+using System.Reflection.Metadata;
+
+// Starter 
+
+/*
+
+Console.WriteLine("What's your favourite animal?"); 
+
+string animal  = Console.ReadLine(); 
+
+Console.WriteLine("What's it typical lifespan?"); 
+
+double lifespan = Convert.ToDouble(Console.ReadLine()); 
+
+double halflife = lifespan / 2; 
+
+Console.WriteLine($"A {animal} typically lives for {lifespan} years, and half of its life is: {halflife} years."); 
+
+*/
+
+
+
+// Converting Miles into KM 
+
+
+
+/*
+
+const double ConvertMilesToKM = 1.6; // Multiplier for Miles to KM 
+
+Console.WriteLine("Enter the amount of miles:"); // Question for user 
+
+double miles = Convert.ToDouble(Console.ReadLine()); // Miles = Input 
+
+double KM = miles * ConvertMilesToKM; // Kilometres = miles multiplied by the Multiplier (ConvertMilesToKM) 
+
+Console.WriteLine($"Miles: {miles}, Kilometres: {KM}"); // Outputs answers 
+
+
+*/
+
+
+// Personal Information 
+
+/* 
+
+Console.WriteLine("Enter your height:"); 
+
+double height  = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("Enter your age:"); 
+
+double age = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("Enter your gender:"); 
+
+string gender = Console.ReadLine(); 
+
+Console.WriteLine($"Height: {height}, Age: {age}, Gender: {gender}.");
+
+string height2 = Convert.ToString(height);
+string age2 = Convert.ToString(age);
+
+string[] PersonalInfo = { height2, age2, gender};
+
+*/
